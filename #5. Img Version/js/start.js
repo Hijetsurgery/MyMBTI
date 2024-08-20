e5f4b4132018ec5d076b9c83e533a709f65ab30d
Hijetsurgery <131605667+Hijetsurgery@users.noreply.github.com>
2024-08-20 15:30:13 +0900

    Update start.js
    1차수정

--- #5. Img Version/js/start.js
+++ #5. Img Version/js/start.js
@@ -2,7 +2,7 @@ const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
 const result = document.querySelector("#result");
 
-const endPoint = 3;
+const endPoint = 12;
 const select = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 function calResult(){
@@ -41,62 +41,50 @@ function goResult(){
     setResult();
 }
 
-function ImageFadeOut(qIdx, idx){
-  var left = document.querySelector('.leftImage');
-  var right = document.querySelector('.rightImage');
-  left.disabled = true;
-  left.classList.remove("fadeIn");
-  left.classList.add("fadeOut");
-  right.disabled = true;
-  right.classList.remove("fadeIn");
-  right.classList.add("fadeOut");
+function addAnswer(answerText, qIdx, idx){
+  var a = document.querySelector('.answerBox');
+  var answer = document.createElement('button');
+  answer.classList.add('answerList');
+  answer.classList.add('my-3');
+  answer.classList.add('py-3');
+  answer.classList.add('mx-auto');
+  answer.classList.add('fadeIn');
 
-  setTimeout(() =>{
-    if(qIdx+1 === endPoint){
-      goResult();
-      return;
-    } else{
-      setTimeout(() => {
-        var target = qnaList[qIdx].a[idx].type;
-        for(let i = 0; i < target.length; i++){
-          select[target[i]] += 1;
-        }
-        goNext(++qIdx);
-      },450);
+  a.appendChild(answer);
+  answer.innerHTML = answerText;
+
+  answer.addEventListener("click", function(){
+    var children = document.querySelectorAll('.answerList');
+    for(let i = 0; i < children.length; i++){
+      children[i].disabled = true;
+      children[i].style.WebkitAnimation = "fadeOut 0.5s";
+      children[i].style.animation = "fadeOut 0.5s";
     }
-  },450)
+    setTimeout(() => {
+      var target = qnaList[qIdx].a[idx].type;
+      for(let i = 0; i < target.length; i++){
+        select[target[i]] += 1;
+      }
 
+      for(let i = 0; i < children.length; i++){
+        children[i].style.display = 'none';
+      }
+      goNext(++qIdx);
+    },450)
+  }, false);
 }
 
 function goNext(qIdx){
+  if(qIdx === endPoint){
+    goResult();
+    return;
+  }
+
   var q = document.querySelector('.qBox');
   q.innerHTML = qnaList[qIdx].q;
-
-  var left = document.querySelector('.leftImage');
-  var right = document.querySelector('.rightImage');
-  var qnaURL = './img/question/';
-  left.src = qnaURL + qIdx + '-A.png';
-  right.src = qnaURL + qIdx + '-B.png';
-
-  try {
-    left.classList.remove("fadeOut");
-    right.classList.remove("fadeOut");
-  } catch (e) {
-    console.log(e);
+  for(let i in qnaList[qIdx].a){
+    addAnswer(qnaList[qIdx].a[i].answer, qIdx, i);
   }
-  left.classList.add("fadeIn");
-  right.classList.add("fadeIn");
-
-  left.addEventListener("click", function(){
-    var target = qnaList[qIdx].a[0].type;
-    ImageFadeOut(qIdx ,0);
-  }, false);
-
-  right.addEventListener("click", function(){
-    var target = qnaList[qIdx].a[1].type;
-    ImageFadeOut(qIdx, 1);
-  }, false);
-
   var status = document.querySelector('.statusBar');
   status.style.width = (100/endPoint) * (qIdx+1) + '%';
 }
